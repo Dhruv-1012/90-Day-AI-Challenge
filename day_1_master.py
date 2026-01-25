2109dae9fb74950bd4bb96bb98316fee0d30ba8d
Dhruv-1012 <dhruv74704@gmail.com>
2026-01-26 02:56:44 +0530

    Mastered Variables, Strings, and Input
    Completed Lectures 2 & 3. Built an interactive script using f-strings and type casting (float/int). Connected local VS Code to GitHub.

---
+++ day_1_master.py
@@ -0,0 +1,4 @@
+name = input("Enter your first and last name : ")
+score = float(input("What is the score of your 1 semester : "))
+score1 = 100 - score
+print(f" Hello {name.title()}:\n You are {score1} away from a perfact 100% score:")
---
+++ day_1_practice.py
@@ -0,0 +1,83 @@
+# LECTURE 2
+#  Here we use type it is a defult funcation that help us to identify what type of value is variable is holding whather it was int or float 
+# a = 2
+# print("the value of a is ",a)
+# print("type ",type (a))
+# b = 45.433
+# print("the value of b is ", b)
+# print("type is ",type(b)) 
+
+# Here we see that python will print the latest value of a variable or print latest declariation of a variable
+# It have auto garbage collection call and delete the old data to free space 
+#  a = 2
+# b = 45.433
+# print("the value of b is ", b)
+# print("type is ",type(b)) 
+# a = 10
+# print("the value of a is ",a)
+# print("type ",type (a))
+
+# And here we see that it print the latest one even the type is change from int to float 
+# a = 2
+# b = 45.433
+# print("the value of b is ", b)
+# print("type is ",type(b)) 
+# a = 10
+# a = 342.33425
+# print("the value of a is ",a)
+# print("type ",type (a))
+
+#Now for string and it do not matter that you use ' ' or " "
+# s = "dhruv"
+# print("",s)
+# a = 'my class is 12th'
+# print("",a)
+# print("",type(s),type(a))
+
+# boolian type
+# b = True    # True internal meaning is 1
+# print("",type(b))
+# c = False  # False internal meaning is 0
+# print("",type(c))
+# print("",b-c)
+# a = 100
+# print("answer is ",a+b)
+# print("answer is",b*c)
+# h = 7 + 8j     # In python or in programing language we use j to denot is as a complex number or imagenary
+# print("h",type(h))
+# i = 6j
+# print("i",type(i))
+# print("",h)
+# print("",h.imag)
+# print("",h.real)
+
+# LECTURE END
+
+# LECTURE 3
+
+s = 'dhruv'
+# print("",type(s))
+s1 = "hello world"
+# print("",type(s1))
+len(s)      # len is a function that tell you the length of the string and it count the characters as well as space like dhru v there are 6 characters including space 
+# print("",len(s))
+# print("",len(s1))
+
+# print("",s.find('h'))   # here it mean that in variable s using the inbuld funcation called find ('h') and typer the character be need to find
+# print("",s.find('H'))       # if data is not available the it return -1
+# print("",s1.find('o'))      # here it will print the first match or print the lowest index find it will print that and not check further
+# print("",s1.find('or'))
+# print("",s1.count('l'))     # it will tell you how many time the character is repeating in the string 
+# print("",s1.count('o')) 
+# print("",s1.count('q')) 
+
+# print("",s1.upper())        # it just convert the whole string in upper case and it did not change the main data it is not permanent
+s3 ='heLLoDhRuv'
+# print("",s3.upper())
+# print("",s3.lower())        # it just conver  the whole string in lower case and it did not change the main data it is not permanent
+# s3 = s3.lower()
+# print("",s3)   #and now it is permanent
+print("",s3.swapcase())   # it just swap the character of the string in lower to upper upper to lower 
+print("",s3.title())        # it will make first latter of each and every string capital or in upper case
+print("",s1.title())
+print("",s1.capitalize())
